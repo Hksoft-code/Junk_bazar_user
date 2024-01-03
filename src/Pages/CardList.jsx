27d6251fdd7fc5de6bda8471638ae934586ae9c0
chandraguptsharma1 <chandraguptsharma1@gmail.com>
2024-01-03 18:19:26 +0530

    cart list track order

--- src/Pages/CardList.jsx
+++ src/Pages/CardList.jsx
@@ -14,54 +14,35 @@ const CartList = () => {
   const dispatch = useDispatch();
   const [scrapList, setScrapList] = useState([]);
   const navigate = useNavigate();
-  const [quantity, setQuantity] = useState({});
+  const [quantity, setQuantity] = useState(1);
   const [currentPage, setCurrentPage] = useState(1);
   const itemsPerPage = 4;
+  const [loading, setLoading] = useState(true);
   const [totalItems, setTotalItems] = useState(0);
   const [totalPages, setTotalPages] = useState(0);
   const [addToCard, setAddToCard] = useState();
+  const [scrapPass, setScrapPass] = useState([]);
   const initialScrapState = {
     scraps: [],
     totalScrapCount: 0,
   };
   async function fetchData(page) {
     try {
-      const response = await axiosInstance.get("/getAddToCart", {
-        params: {
-          page: page - 1,
-          limit: itemsPerPage,
-        },
+      const response = await axiosInstance.get(`/getAddToCart?page=${page - 1}&limit=10`, {
+
       });
 
-      const responseData = response.data;
-      console.log("scrap data", JSON.parse(responseData.data))
-      const parsedData = JSON.parse(responseData.data);
-      if (parsedData && parsedData.cartLists) {
-        const { cartLists, totalScrapCount } = parsedData;
+      const scrapAll = JSON.parse(response.data.data);
+      console.log('orderList', scrapAll);
 
-        setTotalItems(totalScrapCount);
-        setAddToCard(parsedData.cartLists.addToCartId)
-        const initialQuantityState = {};
-        cartLists.items.forEach((item) => {
-          initialQuantityState[item.scrapInfo.scrapId] = item.quantity;
-        });
+      setScrapList(scrapAll.cartLists);
+      console.log("card list", scrapList)
+      // Update the total pages based on the totalScrapCount
+      // const calculatedTotalPages = Math.ceil(scrapList.totalScrapCount / itemsPerPage);
+      setTotalPages("1");
 
-        setQuantity(initialQuantityState);
+      setLoading(false);
 
-        if (page === 1) {
-          setScrapList(cartLists.items);
-        } else {
-          setScrapList((prevScrapList) => [
-            ...prevScrapList,
-            ...cartLists.items,
-          ]);
-        }
-
-        const calculatedTotalPages = Math.ceil(totalScrapCount / itemsPerPage);
-        setTotalPages(calculatedTotalPages);
-      } else {
-        console.error("Invalid response structure:", responseData);
-      }
     } catch (error) {
       console.error("Error fetching data:", error);
     }
@@ -89,7 +70,7 @@ const CartList = () => {
           timer: 2500,
           title: data.message,
         });
-
+        fetchData(1)
         setScrapList((prevScrapList) =>
           prevScrapList.filter((scrap) => scrap.scrapId !== scrapId)
         );
@@ -104,22 +85,52 @@ const CartList = () => {
     }
   };
 
-  const handleRequestAddToCart = async (cart) => {
-    console.log("card dat ", cart)
-    // if (cart.quantity > 0) {
-    console.log("card dat ", cart)
-    const currentQuantity = quantity[cart.scrapInfo.scrapId] || 0;
+  const handlePickeupRequest = async (cart) => {
 
-    if (currentQuantity === 0) {
-      setQuantity((prevQuantity) => ({
-        ...prevQuantity,
-        [cart.scrapInfo.scrapId]: 1,
-      }));
+    console.log("card dat ", cart)
+    const data = cart.items;
+
+
+    for (var i = 0; i < data.length; i++) {
+      const data = cart.item;
+      const scrap = data[i];
+      console.log("scrap id", scrap);
     }
 
+    console.log("scrapList", scrapPass)
+    // if (currentQuantity === 0) {
+    //   setQuantity((prevQuantity) => ({
+    //     ...prevQuantity,
+    //     [cart.scrapInfo.scrapId]: 1,
+    //   }));
+    // }
+
+    // console.log("card", cart);
+
+    // const passData = {
+    //   addToCartId: cart.addToCartId,
+    //   quantity: cart.item[0].quantity,
+    //   price: cart.scrapInfo.price,
+    //   scrapId: cart.scrapInfo.scrapId,
+    //   quantityType: cart.scrapInfo.quantityType,
+    // };
+
+    // console.log("passdata", passData);
+
+    // navigate("/request_pickup", {
+    //   state: {
+    //     passData,
+    //   },
+    // });
+
+
+
+  };
+
+  const addQuantity = async (scrapId, quantity) => {
     const payload = {
-      addScrapQuantity: currentQuantity,
-      scrapId: cart.scrapInfo.scrapId,
+      addScrapQuantity: quantity,
+      scrapId: scrapId,
     };
 
     try {
@@ -132,49 +143,26 @@ const CartList = () => {
     } catch (error) {
       console.error("Error updating scrap quantity:", error);
     }
+  }
 
-    console.log("card", cart)
-
-    const passData = {
-      addToCartId: addToCard,
-      quantity: currentQuantity,
-      price: cart.scrapInfo.price,
-      scrapId: cart.scrapInfo.scrapId,
-      quantityType: cart.scrapInfo.quantityType,
-    };
-
-    console.log("passdata", passData);
-
-    navigate("/request_pickup", {
-      state: {
-        passData,
-      },
-    });
-    // } else {
-    //   Swal.fire({
-    //     icon: "error",
-    //     position: "center",
-    //     showConfirmButton: false,
-    //     timer: 2500,
-    //     title: "Add Quantity",
-    //   });
-    // }
-
-
-  };
 
   const handleIncrement = (scrapId) => {
+
     setQuantity((prevQuantity) => ({
       ...prevQuantity,
       [scrapId]: (prevQuantity[scrapId] || 0) + 1,
     }));
+
+    addQuantity(scrapId, quantity);
   };
 
   const handleDecrement = (scrapId) => {
+    console.log("scrap decrease", scrapId)
     setQuantity((prevQuantity) => ({
       ...prevQuantity,
       [scrapId]: Math.max((prevQuantity[scrapId] || 0) - 1, 0),
     }));
+    addQuantity(scrapId, quantity);
   };
 
   const handlePageChange = (pageNumber) => {
@@ -187,122 +175,91 @@ const CartList = () => {
   const startIndex = (currentPage - 1) * itemsPerPage;
   const endIndex = startIndex + itemsPerPage;
 
-  const currentItems = scrapList.slice(startIndex, endIndex);
+  // const currentItems = scrapList.slice(startIndex, endIndex);
 
   useEffect(() => {
     console.log("Effect triggered for page:", currentPage);
     console.log("Calling fetchData in useEffect");
     fetchData(currentPage);
+    setQuantity(1)
   }, [currentPage]);
 
   return (
     <div className="w-full mt-5 flex justify-center items-center lg:max-w-[1100px] mx-auto ">
       <div className="max-w-screen-xl w-full md:px-2 lg:px-4 px-0 flex-col flex justify-center items-center">
-        {currentItems && currentItems.length > 0 ? (
-          currentItems.map((cart, index) => (
-            <div
-              key={index}
-              className="w-full max-sm:h-[250px] h-[300px] md:h-auto bg-[#80d7421c] mt-[10px] mb-[10px] flex flex-col md:flex-row justify-between items-center p-[2.5rem] py-[2.7rem] md:p-8 lg:p-12"
-            >
-              <div className="flex justify-center items-center mb-4 md:mb-0">
-                <img
-                  className="w-[150px] h-[150px] max-sm:w-[100px] max-sm:h-[100px] object-cover mr-[20px]  max-er:w-[120px] max-er:h-[120px] rounded-[10px]"
-                  src={cart?.docUrl}
-                  alt=""
-                />
-                <div>
-                  <h5 className="opacity-80 text-3xl font-['Gilroy-SemiBold'] text-[#4a4a4a] ">
-                    {cart?.scrapInfo.scrapName}
-                  </h5>
-                  <p className="font-bold text-[10px] max-er:text-[10px] md:text-[14px] text-gray-700">
-                    ₹ {cart?.scrapInfo.price} - {cart?.scrapInfo.quantityType}
-                  </p>
+        {scrapList?.items && scrapList?.items.length > 0 ? (
+          <div class="mx-auto mt-8 max-w-2xl md:mt-12">
+            <div class="bg-white shadow">
+              <div class="px-4 py-6 sm:px-8 sm:py-10">
+                <div class="flow-root">
+                  <ul class="-my-8">
+                    {scrapList?.items.map((cart, index) => (
+                      <li key={index} class="flex flex-col space-y-3 py-6 text-left sm:flex-row sm:space-x-5 sm:space-y-0">
+                        <div class="shrink-0">
+                          <img class="h-24 w-24 max-w-full rounded-lg object-cover" src={cart?.scrapInfo.docUrl} alt="" />
+                        </div>
 
-                  <div className="flex items-start mt-2">
+                        <div class="relative flex flex-1 flex-col justify-between">
+                          <div class="sm:col-gap-5 sm:grid sm:grid-cols-2">
+                            <div class="pr-8 sm:pr-5">
+                              <p class="text-base font-semibold text-gray-900"> {cart?.scrapInfo.scrapName}</p>
+                              {/* <p class="mx-0 mt-1 mb-0 text-sm text-gray-400">{order?.addressInfo.address}</p> */}
+                            </div>
+
+                            <div class="mt-4 flex items-end justify-between sm:mt-0 sm:items-start sm:justify-end">
+                              <p class="shrink-0 w-20 text-base font-semibold text-gray-900 sm:order-2 sm:ml-8 sm:text-right">₹ {cart?.scrapInfo.price}</p>
+
+                              <div class="sm:order-1">
+                                <div class="sm:order-1">
+                                  <div className="flex items-start mt-2">
+                                    <button onClick={() => handleDecrement(cart?.scrapId)} className="border bg-lime-500 text-white rounded-md py-2 px-4 mr-2">-</button>
+                                    <span className="text-4xl font-bold mx-4">{quantity[cart?.scrapId]}</span>
+                                    <button onClick={() => handleIncrement(cart?.scrapId)} className="border bg-lime-500 text-white rounded-md py-2 px-4 ml-2">+</button>
+                                  </div>
+                                </div>
+                              </div>
+
+                              <div class="absolute top-0 right-0 flex sm:bottom-0 sm:top-auto">
+                                <button onClick={(e) => removeFromCard(cart.scrapId)} type="button" class="flex rounded p-2 text-center text-gray-500 transition-all duration-200 ease-in-out focus:shadow hover:text-gray-900">
+                                  <svg class="h-5 w-5" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" class=""></path>
+                                  </svg>
+                                </button>
+                              </div>
+                            </div>
+                          </div>
+
+
+                        </div>
+                      </li>
+                    ))}
+
+
+                  </ul>
+                </div>
+
+
+
+                <div class="mt-6 flex text-center justify-end  space-x-4 border-t border-b py-5">
+                  <div className="flex space-x-4">
                     <button
-                      onClick={() => handleDecrement(cart.scrapId)}
-                      className="border bg-lime-500 text-white rounded-md py-2 px-4 mr-2"
+                      onClick={() => navigate("/pricing", { replace: true })}
+                      className="lg:w-[200px] h-[50px] font-semibold bg-transparent border border-black rounded-[30px] cursor-pointer max-sm:w-[100px] max-er:text-[10px] lg:text-[15px] max-md:w-[120px] max-er:w-[130px] p-3"
                     >
-                      -
+                      Browse More Scraps
                     </button>
-                    <span className="text-4xl font-bold mx-4">
-                      {quantity[cart.scrapId]}
-                    </span>
                     <button
-                      onClick={() => handleIncrement(cart.scrapId)}
-                      className="border bg-lime-500 text-white rounded-md py-2 px-4 ml-2"
+                      onClick={() => handlePickeupRequest(scrapList)}
+                      className="lg:w-[200px] rounded-[30px] h-[50px] font-semibold text-white bg-[#81D742] cursor-pointer max-sm:w-[100px] max-er:text-[10px] lg:text-[15px] max-md:w-[120px] max-er:w-[130px] p-3"
                     >
-                      +
+                      Request Pickup
                     </button>
                   </div>
-                  {/* <button
-                                            onClick={() => handleDecrement(cart.scrapId)}
-                                            className="flex justify-center items-center w- h-10 rounded-full text-white focus:outline-none bg-gray-400 hover:bg-gray-500"
-                                        >
-                                            -
-                                        </button>
-                                        <span className="text-4xl font-bold mx-4">
-                                            {quantity[cart.scrapId]}
-                                        </span>
-                                        <button
-                                            onClick={() => handleIncrement(cart.scrapId)}
-                                            className="flex justify-center items-center w-10 h-10 rounded-full text-white focus:outline-none bg-indigo-500 hover:bg-indigo-600"
-                                        >
-                                            +
-                                        </button> */}
                 </div>
               </div>
-              <div className="flex space-x-4">
-                <button
-                  onClick={() => navigate("/pricing", { replace: true })}
-                  className="lg:w-[200px] h-[50px] font-semibold bg-transparent border border-black rounded-[30px] cursor-pointer max-sm:w-[100px] max-er:text-[10px] lg:text-[15px] max-md:w-[120px] max-er:w-[130px] p-3"
-                >
-                  Browse More Scraps
-                </button>
-                <button
-                  onClick={() => handleRequestAddToCart(cart)}
-                  className="lg:w-[200px] rounded-[30px] h-[50px] font-semibold text-white bg-[#81D742] cursor-pointer max-sm:w-[100px] max-er:text-[10px] lg:text-[15px] max-md:w-[120px] max-er:w-[130px] p-3"
-                >
-                  Request Pickup
-                </button>
-              </div>
-              <div
-                className="cursor-pointer"
-                onClick={() => {
-                  removeFromCard(cart.scrapInfo.scrapId);
-                }}
-              >
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="16"
-                  height="16"
-                  fill="currentColor"
-                  class="bi bi-x-circle"
-                  viewBox="0 0 16 16"
-                >
-                  <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14m0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16" />
-                  <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708" />
-                </svg>
-                <div class="opacity-0 w-28 bg-black text-white text-center text-xs rounded-lg py-2 absolute z-10 group-hover:opacity-100 bottom-full -left-1/2 ml-14 px-3 pointer-events-none">
-                  Remove
-                  <svg
-                    class="absolute text-black h-2 w-full left-0 top-full"
-                    x="0px"
-                    y="0px"
-                    viewBox="0 0 255 255"
-                  >
-                    <polygon
-                      class="fill-current"
-                      points="0,0 127.5,127.5 255,0"
-                    />
-                  </svg>
-                </div>
-              </div>
-
-              {/* <MdDeleteForever className="bg-[#81D742]" onClick={() => { removeFromCard(cart.addToCartId) }} /> */}
-              {/* <button className="bg-[#81D742] p-2 rounded items-end text-white text-right justify-end" onClick={() => { removeFromCard(cart.addToCartId) }}>Cross</button> */}
             </div>
-          ))
+          </div>
+
         ) : (
           <div className="container mx-auto max-w-sm w-full  sm:w-1/2">
             <div className="card flex flex-col justify-center p-10 rounded-lg ">
@@ -335,13 +292,13 @@ const CartList = () => {
             </div>
           </div>
         )}
-        <div className="flex items-center  justify-center   ">
+        {/* <div className="flex items-center  justify-center   ">
           <PaginationComponent
             totalPages={totalPages}
             currentPage={currentPage}
             onPageChange={handlePageChange}
           />
-        </div>
+        </div> */}
       </div>
     </div>
   );
