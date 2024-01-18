a6e9d42fcabe502ac9412d386826419ce050d270
chandraguptsharma1 <chandraguptsharma1@gmail.com>
2024-01-18 23:50:36 +0530

    commit

--- src/Module/summary/Summary.jsx
+++ src/Module/summary/Summary.jsx
@@ -10,7 +10,7 @@ const Summary = () => {
       <div>
         <Nav />
         <div className=" mt-20 lg:mt-32  lg:max-w-[1250px] mx-auto">
-        <Link to="/changeAddress">
+          <Link to="/cart">
             <IoChevronBackOutline className="ml-12 w-12 h-12 cursor-pointer rounded-full border border-gray-300 p-2 hover:bg-gray-100 focus:outline-none focus:border-blue-300 focus:ring focus:ring-blue-200" />
           </Link>
           <h2 className="mt-5 text-3xl text-center text-[#060714] font-['Gilroy-Bold']  font-extrabold">
